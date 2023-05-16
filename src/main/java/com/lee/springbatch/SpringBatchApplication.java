b3bb6d4926addbd58741df01985405858a2fa532
chanboklee <chanboklee@naver.com>
2023-05-16 15:08:03 +0900

    Fix : spring-batch - JobParameter

--- src/main/java/com/lee/springbatch/SpringBatchApplication.java
+++ src/main/java/com/lee/springbatch/SpringBatchApplication.java
@@ -1,8 +1,10 @@
 package com.lee.springbatch;
 
+import com.lee.springbatch.jobparameter.JobParameterTest;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableBatchProcessing
--- src/main/java/com/lee/springbatch/jobinstance/JobInstanceConfiguration.java
+++ src/main/java/com/lee/springbatch/jobinstance/JobInstanceConfiguration.java
@@ -1,4 +1,4 @@
-package com.lee.springbatch;
+package com.lee.springbatch.jobinstance;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
