160d15323881bd435716c7acb51196a85f7889be
chanboklee <chanboklee@naver.com>
2023-05-20 12:17:15 +0900

    Fix : spring-batch - ExecutionContext

--- src/main/java/com/lee/springbatch/SpringBatchApplication.java
+++ src/main/java/com/lee/springbatch/SpringBatchApplication.java
@@ -4,9 +4,12 @@ import com.lee.springbatch.jobparameter.JobParameterTest;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.lee.springbatch.executioncontext"})
 @EnableBatchProcessing
 public class SpringBatchApplication {
 
